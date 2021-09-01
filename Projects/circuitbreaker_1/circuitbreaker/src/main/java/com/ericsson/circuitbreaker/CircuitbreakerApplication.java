30ac94bade71607b1b4ada63616fbe45aa6379ca
sanapala12 <59277514+sanapala12@users.noreply.github.com>
2021-09-01 18:52:33 +0530

    Add files via upload

---
+++ Projects/circuitbreaker_1/circuitbreaker/src/main/java/com/ericsson/circuitbreaker/CircuitbreakerApplication.java
@@ -0,0 +1,19 @@
+package com.ericsson.circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+
+public class CircuitbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitbreakerApplication.class, args);
+	}
+
+}
---
+++ Projects/circuitbreaker_1/circuitbreaker/src/main/java/com/ericsson/circuitbreaker/controllers/CBController.java
@@ -0,0 +1,28 @@
+package com.ericsson.circuitbreaker.controllers;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ericsson.circuitbreaker.handlers.CBHandler;
+
+
+
+
+@RestController
+public class CBController {
+    @Autowired
+	private CBHandler cbHandler;
+	@GetMapping("/gateways/{userName}/{password}")
+	public ResponseEntity<?> invokeAPIGeteway(@PathVariable("userName") String userName, @PathVariable("password") String password) {
+		
+		return this.cbHandler.requestHandler(userName,password);
+		
+	}
+}
